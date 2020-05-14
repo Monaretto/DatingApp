76fbad03c9c06357d930d27b4fbbb03bf258110d
Vinicius Reginato Monaretto (Elipse Software) <reginato@elipse.com.br>
2020-05-14 11:04:11 -0300

    Nav and Auth

---
+++ DatingApp-SPA/src/app/Nav/Nav.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import {AuthService} from './../_Services/auth.service';
+
+@Component({
+  // tslint:disable-next-line:component-selector
+  selector: 'app-Nav',
+  templateUrl: './Nav.component.html',
+  styleUrls: ['./Nav.component.css']
+})
+export class NavComponent implements OnInit {
+
+  model: any = {};
+
+  constructor(private authService: AuthService) { }
+
+  ngOnInit() {
+  }
+
+  login() {
+    this.authService.login(this.model).subscribe(next => {
+      console.log('login is sucessfull');
+    }, error => {
+      console.log('login error');
+    });
+  }
+
+}
