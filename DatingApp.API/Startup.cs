27d771f4b6577b7ed32c529fceab423b566355a7
Monaretto <vini.regimona@gmail.com>
2020-05-19 16:33:02 -0300

    End of Section 8

--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -19,6 +19,7 @@ using System.Net;
 using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Http;
 using DatingApp.API.helpers;
+using AutoMapper;
 
 namespace DatingApp.API
 {
@@ -35,9 +36,15 @@ namespace DatingApp.API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<DataContext>(x => x.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddControllers().AddNewtonsoftJson(opt => {
+                opt.SerializerSettings.ReferenceLoopHandling = 
+                Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+            });
             services.AddScoped<IAuthRepository, AuthRepository>();
-            services.AddControllers();
+            services.AddControllers().AddNewtonsoftJson();
             services.AddCors();
+            services.AddAutoMapper(typeof(DatingRepository).Assembly);
+            services.AddScoped<IDatingRepository, DatingRepository>();
             services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
             .AddJwtBearer(options=>{
                 options.TokenValidationParameters = new TokenValidationParameters{
--- DatingApp.API/helpers/extension.cs
+++ DatingApp.API/helpers/extension.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Http;
+using System;
 
 namespace DatingApp.API.helpers
 {
@@ -10,5 +11,18 @@ namespace DatingApp.API.helpers
             response.Headers.Add("Access-Control-Expose-Headers","ApplicatioN-Error");
             response.Headers.Add("Access-Control-Allow-Origin","*");        
         }
+
+        public static int CalculateAge(this DateTime theDateTime)
+        {
+            var age = DateTime.Today.Year - theDateTime.Year;
+            if(theDateTime.AddYears(age) > DateTime.Today)
+            {
+                age --;
+            }
+
+            return age;
+        }
     }
+
+    
 }
